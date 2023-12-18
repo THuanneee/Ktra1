221b50f8cf80e99d69766e0c73f8a6fe295a9904
Levi <phamnguyentanphucgl@gmail.com>
2023-12-18 22:43:16 +0700

    build ui login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.levi.employeemanager"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.levi.employeemanager"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
